aa6e660954ba64d65a061e86e5dcc335085bf540
Arnel <alopena55555@gmail.com>
2025-01-28 21:04:34 +0800

    added cron on initialize

--- init.ts
+++ init.ts
@@ -4,14 +4,16 @@ import { exit } from "process";
 import { flushRules } from "./lib/flushRules";
 import { checkRule } from "./lib/checkRule";
 import { execSync } from "child_process";
+import { createJob } from "./lib/jobs";
 
 async function init() {
     try {
-       const [rows] = await db.query<RowDataPacket[]>('SELECT * FROM users WHERE expire_on < NOW();');
+       const [rows] = await db.query<RowDataPacket[]>('SELECT * FROM users WHERE expire_on >= NOW();');
        rows.forEach(async (row) => {
             flushRules(row.mac);
             if (!checkRule(row.mac)) {
                 execSync(`iptables -I FORWARD -i enx00e0990026d3 -o end0 -m mac --mac-source ${row.mac} -j ACCEPT`);
+                createJob(row.mac, row.expire_on);
             }
        });
     console.log(rows);
