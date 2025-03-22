3d3f91d0f2166520ad30366fac756201e5357fc9
nel003 <37954915+nel003@users.noreply.github.com>
2025-03-22 22:31:19 +0800

    Update route.ts

--- app/redirect/route.ts
+++ app/redirect/route.ts
@@ -1,5 +1,6 @@
-
 export async function GET(req: Request) {
-    const ref = req.headers.get("Referrer");
-    return new Response(null, {status: 302, headers: {Location: process.env.MAIN_URL+`?from=${ref}` || ""}});
+    const ref = req.headers.get("Referrer") || "";
+    const redirectUrl = process.env.MAIN_URL ? `${process.env.MAIN_URL}?from=${encodeURIComponent(ref)}` : "/";
+
+    return new Response(null, { status: 302, headers: { Location: redirectUrl } });
 }
