57e482aeaf38732dbfb33167a1db226466e55115
nel <alopena55555@gmail.com>
2025-12-27 05:00:40 +0000

    rebuil

--- .next/types/app/api/admin/dashboard/route.ts
+++ .next/types/app/api/admin/dashboard/route.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\bispl\OneDrive\Desktop\arns\wifi-vendo\app\api\admin\dashboard\route.ts
+// File: /root/wifi-vendo/app/api/admin/dashboard/route.ts
 import * as entry from '../../../../../../app/api/admin/dashboard/route.js'
 import type { NextRequest } from 'next/server.js'
 
